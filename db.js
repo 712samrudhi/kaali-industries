1c1b61c9ea7de2c373c9be4ba1465706d120a0c5
Samrudhi Sawant <samrudhi731@gmail.com>
2026-06-25 12:30:48 +0530

    fix: correct db host

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "auth-db1877.hstgr.io",
     user: "u108577261_nutrientuser",
     password: "Nutrient@07",
     database: "u108577261_nutrient",
