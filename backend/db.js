6750a1793dc44a05cd757735e6b11d7a5a3be25f
Samrudhi Sawant <samrudhi731@gmail.com>
2026-06-25 13:09:20 +0530

    fix: update db password

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "auth-db1877.hstgr.io",
     user: "u108577261_nutrientuser",
-    password: "Prasam&712",
+    password: "Nutrient123456",
     database: "u108577261_nutrient",
     port: 3306
 });
