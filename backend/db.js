7be3c67b200fc81d0b63016c90bf63ee4bec53a8
Samrudhi Sawant <samrudhi731@gmail.com>
2026-06-25 11:52:01 +0530

    fix: db env debug

--- backend/db.js
+++ backend/db.js
@@ -2,16 +2,19 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "127.0.0.1",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "nutrient",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
     if (err) {
         console.log("DB Connection Failed ❌", err);
+        console.log("HOST:", process.env.DB_HOST);
+        console.log("USER:", process.env.DB_USER);
+        console.log("DB:", process.env.DB_NAME);
     } else {
         console.log("DB Connected Successfully ✅");
     }
