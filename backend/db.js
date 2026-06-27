1c03de933a7892bcfaacb7dd936df0fdf309c3fc
Samrudhi Sawant <samrudhi731@gmail.com>
2026-06-27 16:37:53 +0530

    fix: use connection pool to fix ECONNRESET

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,24 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: "srv1877.hstgr.io",
     user: "u108577261_nutrientuser",
     password: "Nutrient123456",
     database: "u108577261_nutrient",
-    port: 3306
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if (err) {
         console.log("DB Connection Failed ❌", err);
     } else {
         console.log("DB Connected Successfully ✅");
+        connection.release();
     }
 });
 
